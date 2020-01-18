fb3dfe12d1f633b742ed9f698cacfef5d2f50af3
ioannisgeor94@gmail.com <ioannisgeor94@gmail.com>
2020-01-18 14:09:00 +0200

    Wrapped with flat list

--- src/screens/profileFlow/OwnerProfileScreen.js
+++ src/screens/profileFlow/OwnerProfileScreen.js
@@ -1,33 +1,50 @@
 import React, { useEffect, useState } from "react";
-import { View } from "react-native";
-import { Text, ButtonGroup } from "react-native-elements"
+import { View, FlatList } from "react-native";
+import { Text, ButtonGroup, ListItem } from "react-native-elements"
 import { connect } from "react-redux";
 import styles from "./OwnerProfileScreen.style"
 import { imageUrl } from "../../api/request";
 import OwnerAvatar from "../../components/OwnerAvatar";
 import PostCard from "../../components/PostCard";
+import { List } from "native-base";
 
-const OwnerProfileScreen = ({ navigation, profile }) => {
+
+const OwnerProfileScreen = ({ navigation, profile, images }) => {
     const [avatarSrc, setAvatarSrc] = useState("../../../resources/images/facebook.jpeg");
 
     /// TODO CREATE SEPARATE HOOK TO DOWNLOAD IMAGES
     useEffect(() => {
         setAvatarSrc(imageUrl + profile.avatar);
     }, [profile]);
-
+    const renderItem = ({ item }) => {
+        return <PostCard />
+    }
+    const renderHeader = () => {
+        return (
+            <View style={{alignItems:'center'}}>
+                <OwnerAvatar
+                    avatarSrc={avatarSrc}
+                    uploaderEmail={profile.email}
+                />
+                <Text h3>{profile.alias}</Text>
+                <ButtonGroup
+                    buttons={buttons}
+                    selectedIndex={0}
+                />
+                <PostCard />
+            </View>
+        )
+    }
     const buttons = ['Images', 'Notifications']
     return (
         <View style={styles.wrapper}>
-            <OwnerAvatar
-                avatarSrc={avatarSrc}
-                uploaderEmail={profile.email}
+
+            <FlatList
+                keyExtractor={(item, index) => { return index }}
+                ListHeaderComponent ={renderHeader}
+                data={images}
+                renderItem={renderItem}
             />
-            <Text h3>{profile.alias}</Text>
-            <ButtonGroup
-                buttons={buttons}
-                selectedIndex={0}
-            />
-            <PostCard />
         </View>
     )
 }
@@ -36,6 +53,7 @@ const mapStateToProps = (state) => {
     console.log(state.owner);
     return {
         profile: state.owner.profile,
+        images: state.owner.images
     }
 }
 
